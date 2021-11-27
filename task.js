5f6dfbeced128a9b9a089b135ea0a09fe30cf675
Olga Petrovich <o.ptrvch@yandex.ru>
2021-11-27 14:09:09 +0300

    added files with tasks

---
+++ task.js
@@ -0,0 +1,19 @@
+//сложение различных типов
+console.log("str" + true);
+console.log("str" + 1);
+console.log(1 + true);
+//умножение различных типов
+console.log("6" * true);
+console.log("6" * 1);
+console.log(1 * true);
+//деление различных типов
+console.log("6" / true);
+console.log("6" / 1);
+console.log(1 / true);
+//явное преобразование
+console.log(String(true));
+console.log(String(2));
+console.log(Number("123"));
+console.log(Number(true));
+console.log(Boolean("123"));
+console.log(Boolean(""));
